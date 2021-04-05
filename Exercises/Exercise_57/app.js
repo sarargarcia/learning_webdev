84e9885c9005420ee2de7099e418d4d799a41470
Sara Garcia <garciasara980@gmail.com>
2021-04-05 18:43:58 +0100

    update exercise 57

---
+++ Exercises/Exercise_57/app.js
@@ -0,0 +1,9 @@
+const li = document.querySelectorAll("li")
+
+for (let i = 0; i < li.length; i++) {
+
+    const li = document.querySelectorAll("li")[i]
+
+     li.classList.toggle("highlight")[i]
+
+}
