6d537b73bbb29b4fa04de25cfd87cf9bd04a3c74
Sara Garcia <garciasara980@gmail.com>
2021-04-07 16:15:30 +0100

    update exercie 58

---
+++ Exercises/Exercise_58/app.js
@@ -0,0 +1,5 @@
+for (var i = 1; i <= 100 ; i++) {
+    let buttonNew = document.createElement("button");
+    buttonNew.innerText = "Hey!";
+    document.getElementById("container").appendChild(buttonNew)
+    }
