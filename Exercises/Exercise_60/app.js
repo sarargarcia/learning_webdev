3516952cb613a9a9f73cd48e129e5efbea5a883d
Sara Garcia <garciasara980@gmail.com>
2021-04-08 16:09:09 +0100

    update  exercise 59 & 60

---
+++ Exercises/Exercise_60/app.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('#hello');
+btn.addEventListener('click', function () {
+    console.log("hello");
+});
+
+const btn1 = document.querySelector('#goodbye');
+btn1.addEventListener('click', function () {
+    console.log("goodbye");
+});
